68434f8f40e3629ec0e80103f634be2087479f7c
Matheus Araujo <97922425+MatzAraujo@users.noreply.github.com>
2022-04-16 00:54:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
-// ABRE E FECHA O MENU QUANDO CLICAR NO ÍCONE
-
-const nav = document.querySelector('#header nav') 
-const toggle= document.querySelectorAll('nav .toggle')  
-
-for(const element of toggle){
-  element.addEventListener('click',function() {
-     nav.classList.toggle('show')
-  })
-}
-
-//    QUANDO CLICAR EM UM ITEM DO MENU,ESCONDER O  MENU
-
-const links = document.querySelectorAll('nav ul li a')
-for(const link of links){
-    link.addEventListener('click',function() {
- nav.classList.remove('show')
-    })
-}
 
 
+   // Quando clicar no menu Hambúrguer, ele irá abrir uma navegação lateral no canto direito da página  
+
+  const menuToggle = document.querySelector('.toggle') 
+  const navshow = document.querySelector('.container') 
+
+  menuToggle.addEventListener('click', () =>{
+     menuToggle.classList.toggle('show')
+     navshow.classList.toggle('show')
+ })
 
